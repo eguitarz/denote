44653aab3e41e0507d4ac01b88c1fc7422e612be
Dale Ma <dalema22@gmail.com>
2015-12-01 02:03:47 -0800

    :wrench: ace-editor

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  didInsertElement: function() {
+    console.log('test');
+    // var editor = ace.edit("editor");
+  },
+  activate: function() {
+    // var editor = ace.edit("editor");
+  }
+});
