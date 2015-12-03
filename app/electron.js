e17cd3dd0777b19766637cc3394815c2bc49a200
Dale Ma <dalema22@gmail.com>
2015-12-03 00:47:06 -0800

    relocate electron.js

--- app/electron.js
+++ app/electron.js
@@ -1,6 +1,7 @@
 const BrowserWindow = require('browser-window');
 const app = require('app');
 const ipc = require('ipc');
+
 const fs = require('fs');
 var mainWindow = null;
 
@@ -20,9 +21,9 @@ app.on('ready', function onReady() {
 
     if (process.env.ELECTRON_ENV === 'development') {
         //mainWindow.openDevTools();
-        mainWindow.loadUrl('http://localhost:5000');
+        mainWindow.loadURL('http://localhost:5000');
     } else {
-        mainWindow.loadUrl('file://' + __dirname + '/dist/index.html');
+        mainWindow.loadURL('file://' + __dirname + '/../dist/index.html');
     }
 
     mainWindow.on('closed', function onClosed() {
