e7a779147e125d6d7e79d309088f585701fc53b2
Dale Ma <dalema22@gmail.com>
2015-12-06 12:13:20 -0800

    added side note page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 const ipc = require('electron').ipcRenderer;
 
-const { set } = Ember;
+const { set, RSVP } = Ember;
+const { hash } = RSVP;
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      mainContent: '',
+      sideNotes: []
+    };
+  },
+
   actions: {
     setEditor(element) {
       let editor = new MediumEditor(element);
@@ -16,7 +24,14 @@ export default Ember.Route.extend({
     },
     createSideNote(timestamp, html) {
       const range = window.getSelection().getRangeAt(0);
-      console.log('createing side note for selected ', range, html);
+      let controller = this.controller;
+      controller.get('model.sideNotes').pushObject({
+        range: range,
+        html: '<div>This is a side note</div>',
+        createdAt: +moment()
+      });
+      console.log(controller.get('model'));
+      // console.log('createing side note for selected ', range, html);
     }
   }
 });
