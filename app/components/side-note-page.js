3f7198bb9b73e6f39d08450e4b1cbbc031efcbe2
Dale Ma <dalema22@gmail.com>
2015-12-16 01:07:15 -0800

    default side note editor

--- app/components/side-note-page.js
+++ app/components/side-note-page.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['side-note-page']
+  classNames: ['side-note-page'],
+
+  didInsertElement() {
+    this.send('createEditor', this.$('#side-note-default-editor'));
+  },
+
+  actions: {
+    createEditor(element) {
+      console.log(element)
+      return new MediumEditor(element, {
+        placeholder: {
+          text: 'Type New Comment'
+        }
+      });
+    }
+  }
 });
