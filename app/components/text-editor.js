5d001f5457a65d63be92121033a62f2567a38926
Dale Ma <dalema22@gmail.com>
2015-12-25 02:27:43 -0800

    add heading buttons

--- app/components/text-editor.js
+++ app/components/text-editor.js
@@ -6,9 +6,38 @@ const ipc = require('electron').ipcRenderer;
 const SAVING_GAP_TIME = 3000;
 const EDITOR_CLASS='.text-editor';
 
+const customizedButtonHTML = `
+  <button type="button" class="h1 heading" data-attribute="h1" data-key="1" aria-label="Heading level 1">H1</button>
+  <button type="button" class="h2 heading" data-attribute="h2" data-key="2" aria-label="Heading level 2">H2</button>
+`;
+
 export default Ember.Component.extend({
-  classNames: ['text-editor-wrapper'],
+  classNames: ['editor'],
+
+  // custom config goes here
+  config: {
+    blockAttributes: {
+      h1: {
+        tagName: 'h1',
+      },
+      h2: {
+        tagName: 'h2',
+      }
+    },
+    textAttributes: {
+      comment: {
+        tagName: 'com'
+      }
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    this._insertCustomButtons();
+  },
+
   didInsertElement() {
+    this._super(...arguments);
 
     ipc.on('local-shortcut-save-file', () => {
       let element = this.$(EDITOR_CLASS)[0];
@@ -23,6 +52,12 @@ export default Ember.Component.extend({
       //           +moment(),
       //           id);
     });
+  },
+
+  _insertCustomButtons() {
+    let groupElement = Trix.config.toolbar.content.querySelector('.block_tools');
+    groupElement.insertAdjacentHTML('afterbegin', customizedButtonHTML);
+    console.log(groupElement)
   },
 
   actions: {
